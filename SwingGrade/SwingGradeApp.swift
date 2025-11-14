67d6596339cac47f3ffe3b9d71fa5006966a311a
Adrian Alvarez <atalvarez77@gmail.com>
2025-11-13 20:51:56 -0600

    Initial Commit

---
+++ SwingGrade/SwingGradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingGradeApp.swift
+//  SwingGrade
+//
+//  Created by Adrian Alvarez on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwingGradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
